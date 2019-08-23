2d47ad762e43c3e62fdb25ac64d3a6846346a0a0
Sadi Mohammad Zaman <sadi@coderex.co>
2019-08-23 20:17:03 +0600

    added

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-let wpUrl = 'https://wordpress-140306-965573.cloudwaysapps.com/wp-json';
+let wpUrl = 'https://wordpress.org/news/wp-json';
 
 // If we're running on Docker, use the WordPress container hostname instead of localhost.
 if (process.env.HOME === '/home/node') {
