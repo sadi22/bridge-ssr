a85ef952e2edf4e360fd85bf70cee8ff906c3925
Sadi Mohammad Zaman <sadi@coderex.co>
2019-08-23 21:20:42 +0600

    added

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -19,21 +19,21 @@ const getSlug = url => {
 class Footer extends Component{
     render() {
         const { logo, menu } = this.props;
-        const menuItems = menu.items.map(item => {
-            const slug = getSlug(item.url);
-            const actualPage = item.object === 'category' ? 'category' : 'post';
-            return (
-                <li key={item.ID}>
-                    <Link
-                        as={`/${item.object}/${slug}`}
-                        href={`/${actualPage}?slug=${slug}&apiRoute=${item.object}`}
-                        key={item.ID}
-                    >
-                        <a>{item.title}</a>
-                    </Link>
-                </li>
-            );
-        });
+        // const menuItems = menu.items.map(item => {
+        //     const slug = getSlug(item.url);
+        //     const actualPage = item.object === 'category' ? 'category' : 'post';
+        //     return (
+        //         <li key={item.ID}>
+        //             <Link
+        //                 as={`/${item.object}/${slug}`}
+        //                 href={`/${actualPage}?slug=${slug}&apiRoute=${item.object}`}
+        //                 key={item.ID}
+        //             >
+        //                 <a>{item.title}</a>
+        //             </Link>
+        //         </li>
+        //     );
+        // });
         return (
           <Fragment>
             <Head>
@@ -53,14 +53,13 @@ class Footer extends Component{
                                         <a><img src={logo}/></a>
                                     </Link>
                                 ) : '' }
-                            </div>    
+                            </div>
                         </div>
                         <div className="col-lg-6">
                             <div className="footer-menu text-center">
                                 <ul>
-                                    {menuItems}
                                 </ul>
-                            </div> 
+                            </div>
                         </div>
                         <div className="col-lg-3">
                             <div className="footer-social-contact text-center">
@@ -68,10 +67,10 @@ class Footer extends Component{
                                     <li><a href="" title="Facebook"><FontAwesomeIcon icon={["fab", "facebook-f"]} /></a></li>
                                     <li><a href="" title="Linked-in"><FontAwesomeIcon icon={["fab", "linkedin-in"]} /></a></li>
                                 </ul>
-                            </div> 
+                            </div>
                         </div>
                     </div>
-                    
+
                     <div className="row copyright-text text-center">
                         <div className="col-12">
                             <p>&copy; Copyright 2019 Bridge</p>
