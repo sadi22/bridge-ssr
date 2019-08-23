5427773c9ca547893616d372b216ca837af8bd4e
Sadi Mohammad Zaman <sadi@coderex.co>
2019-08-23 21:01:48 +0600

    added

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-let wpUrl = 'https://wordpress.org/news/wp-json';
+let wpUrl = 'https://lincolnislam.pro/wp-json';
 
 // If we're running on Docker, use the WordPress container hostname instead of localhost.
 if (process.env.HOME === '/home/node') {
