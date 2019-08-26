37d8f457ba2e9b5692f13543d3f765a5b5742692
Sadi Mohammad Zaman <sadi@coderex.co>
2019-08-26 19:00:17 +0600

    added

--- pages/_error.js
+++ pages/_error.js
@@ -28,8 +28,7 @@ class ErrorPage extends React.Component {
       case 404:
         response = (
           <div>
-            <Head>
-            </Head>
+
             <Container className="pt-5 text-center">
               <h1 className="display-4">Page Not Found</h1>
               <p>The page <strong>{ this.props.router.pathname }</strong> does not exist.</p>
@@ -41,9 +40,7 @@ class ErrorPage extends React.Component {
       case 500:
         response = (
           <div>
-            <Head>
-              <style dangerouslySetInnerHTML={{__html: Styles}}/>
-            </Head>
+
             <Container className="pt-5 text-center">
               <h1 className="display-4">Internal Server Error</h1>
               <p>An internal server error occurred.</p>
@@ -54,9 +51,7 @@ class ErrorPage extends React.Component {
       default:
         response = (
           <div>
-            <Head>
-              <style dangerouslySetInnerHTML={{__html: Styles}}/>
-            </Head>
+
             <Container className="pt-5 text-center">
               <h1 className="display-4">HTTP { this.props.errorCode } Error</h1>
               <p>
