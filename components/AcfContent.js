e292a1f7cc2ce3bc1acff5343a49dfac9fcbd0a2
Sadi Mohammad Zaman <sadi@coderex.co>
2019-08-26 19:04:03 +0600

    added

--- components/AcfContent.js
+++ components/AcfContent.js
@@ -19,12 +19,12 @@ class ACFCONTENT extends Component {
                 return (
                     <Fragment key={section.acf_fc_layout}>
                         {section.acf_fc_layout === 'banner' ? <Banner {...section}/> : ''}
-                        // {section.acf_fc_layout === 'title_and_two_image_columns' ? <SeamlessEcommerce {...section}/> : ''}
-                        // {section.acf_fc_layout === 'title_and_image_with_text_columns' ? <RetailEcommerce {...section}/> : ''}
-                        // {section.acf_fc_layout === 'features' ? <Feature {...section}/> : ''}
-                        // {section.acf_fc_layout === 'overview_with_image' ? <OverView {...section}/> : ''}
-                        // {section.acf_fc_layout === 'feature_box_with_column' ? <DriveSales {...section}/> : ''}
-                        // {section.acf_fc_layout === 'bottom_cta' ? <BottomCTA {...section}/> : ''}
+                        {section.acf_fc_layout === 'title_and_two_image_columns' ? <SeamlessEcommerce {...section}/> : ''}
+                        {section.acf_fc_layout === 'title_and_image_with_text_columns' ? <RetailEcommerce {...section}/> : ''}
+                        {section.acf_fc_layout === 'features' ? <Feature {...section}/> : ''}
+                        {section.acf_fc_layout === 'overview_with_image' ? <OverView {...section}/> : ''}
+                        {section.acf_fc_layout === 'feature_box_with_column' ? <DriveSales {...section}/> : ''}
+                        {section.acf_fc_layout === 'bottom_cta' ? <BottomCTA {...section}/> : ''}
                     </Fragment>
                 );
             });
