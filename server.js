ee3ea471901510432354cae80e088ada0d67c224
Sadi Mohammad Zaman <sadi@coderex.co>
2019-08-26 17:11:33 +0600

    added

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const next = require('next');
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
+var port = process.env.PORT || 8080;
 const handle = app.getRequestHandler();
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 app
   .prepare()
@@ -38,7 +40,7 @@ app
       return handle(req, res);
     });
 
-    server.listen(3000, err => {
+    server.listen(port, err => {
       if (err) throw err;
       console.log('> Ready on http://localhost:3000');
     });
