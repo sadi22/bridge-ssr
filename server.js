2ecf3300c328b8b34c65b03174b151349791bce4
Sadi Mohammad Zaman <sadi@coderex.co>
2019-08-30 11:27:03 +0600

    addd

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const next = require('next');
 var compression = require('compression');
+const LRUCache = require('lru-cache');
+
 
 const dev = process.env.NODE_ENV !== 'production';
 const app = next({ dev });
@@ -8,37 +10,60 @@ const handle = app.getRequestHandler();
 var port = process.env.PORT || 8080;
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
+
+// This is where we cache our rendered HTML pages
+const ssrCache = new LRUCache({
+  max: 100 * 1024 * 1024, /* cache size will be 100 MB using `return n.length` as length() function */
+  length: function (n, key) {
+      return n.length
+  },
+  maxAge: 1000 * 60 * 60 * 24 * 30
+});
+
 app
   .prepare()
   .then(() => {
     const server = express();
-    server.use(compression())
+    server.use(compression());
+
+    server.get('/_next/*', (req, res) => {
+        handle(req, res);
+    });
+
     server.get('/post/:slug', (req, res) => {
       const actualPage = '/post';
       const queryParams = { slug: req.params.slug, apiRoute: 'post' };
-      app.render(req, res, actualPage, queryParams);
+      renderAndCache(req, res, actualPage, queryParams);
     });
 
     server.get('/page/:slug', (req, res) => {
       const actualPage = '/page';
       const queryParams = { slug: req.params.slug, apiRoute: 'page' };
-      app.render(req, res, actualPage, queryParams);
+      renderAndCache(req, res, actualPage, queryParams);
     });
 
     server.get('/category/:slug', (req, res) => {
       const actualPage = '/category';
       const queryParams = { slug: req.params.slug };
-      app.render(req, res, actualPage, queryParams);
+      renderAndCache(req, res, actualPage, queryParams);
     });
 
     server.get('/_preview/:id/:wpnonce', (req, res) => {
       const actualPage = '/preview';
       const queryParams = { id: req.params.id, wpnonce: req.params.wpnonce };
-      app.render(req, res, actualPage, queryParams);
+      renderAndCache(req, res, actualPage, queryParams);
     });
 
+
     server.get('*', (req, res) => {
-      return handle(req, res);
+      if (req.url.includes('/sw')) {
+        const filePath = join(__dirname, 'static', 'workbox', 'sw.js');
+        app.serveStatic(req, res, filePath);
+      } else if (req.url.startsWith('static/workbox/')) {
+        app.serveStatic(req, res, join(__dirname, req.url));
+      } else {
+        handle(req, res, req.url);
+      }
     });
 
     server.listen(port, err => {
@@ -50,3 +75,36 @@ app
     console.error(ex.stack);
     process.exit(1);
   });
+
+
+/*
+ * NB: make sure to modify this to take into account anything that should trigger
+ * an immediate page change (e.g a locale stored in req.session)
+ */
+function getCacheKey(req) {
+  return `${req.path}`
+}
+
+async function renderAndCache(req, res, actualPage, queryParams) {
+  const key = getCacheKey(req);
+
+  if (ssrCache.has(key)) {
+      res.setHeader('x-cache', 'HIT');
+      res.send(ssrCache.get(key));
+      return
+  }
+
+  try {
+      const html = await app.renderToHTML(req, res, actualPage, queryParams);
+
+      if (res.statusCode !== 200) {
+          res.send(html);
+          return
+      }
+      ssrCache.set(key, html);
+      res.setHeader('x-cache', 'MISS');
+      res.send(html)
+  } catch (err) {
+      app.renderError(err, req, res, req.path, req.query)
+  }
+}
