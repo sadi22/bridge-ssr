1008c607596f0eee339a44a5a1959528e478c4b9
Sadi Mohammad Zaman <sadi@coderex.co>
2019-09-05 18:04:02 +0600

    added

--- server.js
+++ server.js
@@ -112,7 +112,7 @@ app
       }
     });
 
-    server.listen(3000, err => {
+    server.listen(port, err => {
       if (err) throw err;
       console.log(`> Ready on http://localhost:${port}`);
     });
