78c69d8e54455b1b8f8ad78849f4ff9188fb53b3
coderexbelal <belal@coderex.co>
2019-09-05 11:52:07 +0600

    font added

--- server.js
+++ server.js
@@ -73,7 +73,7 @@ app
       }
     });
 
-    server.listen(port, err => {
+    server.listen(3000, err => {
       if (err) throw err;
       console.log('> Ready on http://localhost:3000');
     });
