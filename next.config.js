56265fd6461e0b4082bede94c328b155cbbb27d1
Sadi Mohammad Zaman <sadi@coderex.co>
2019-09-13 11:21:10 +0600

    added

--- next.config.js
+++ next.config.js
@@ -1,8 +1,35 @@
+const path = require('path');
 const withSass = require('@zeit/next-sass');
-const withCSS = require('@zeit/next-css');
+const withCSS = require('@zeit/next-css')
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
-module.exports = withCSS(withSass({}));
+const nextConfig = withCSS(withSass({
+  webpack(config, options) {
+    config.module.rules.push({
+      test: /\.(raw)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+      use: 'raw-loader',
+    });
+    config.module.rules.push({
+      type: 'javascript/auto',
+      test: /\.modernizrrc(\.json)?$/,
+      use: ['expose-loader?Modernizr', 'modernizr-loader', 'json-loader'],
+    });
+    config.resolve = {
+      alias: {
+        modernizr$: path.resolve(__dirname, ".modernizrrc.json")
+      }
+    };
+    if (config.mode === 'production') {
+      if (Array.isArray(config.optimization.minimizer)) {
+        config.optimization.minimizer.push(new OptimizeCSSAssetsPlugin({}));
+      }
+    }
+    return config;
+  }
+}));
 
+
+module.exports = nextConfig;
 
 // const path = require('path');
 // const { PHASE_PRODUCTION_SERVER } = require("next-server/constants");
